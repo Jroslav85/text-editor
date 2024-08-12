95547a675824df4b42171bf2f69c5f73a05513a8
Jroslav Trofimov <Jroslav1985@gmail.com>
2024-08-12 17:53:05 +0300

    begin progect

---
+++ src/main/java/home/jroslav/text_editor/App.java
@@ -0,0 +1,13 @@
+package home.jroslav.text_editor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
